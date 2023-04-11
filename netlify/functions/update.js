c5a79e55d143e106a32c2620c4f8f79ba1bed343
Al pacino <ammaryasser7654@gmail.com>
2023-04-11 18:55:46 +0000

    updated

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -12,7 +12,7 @@ const {
 const serverless = require("serverless-http")
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, {});
