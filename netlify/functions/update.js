9a0fb54e40cbc893739d7f4d2965bc95a4047e0f
Al pacino <ammaryasser7654@gmail.com>
2023-04-11 17:22:26 +0000

    updated

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -9,18 +9,17 @@ const {
   sendPage,
   sendPageTafsir,
 } = require("./../../quran-api.js");
+const serverless = require("serverless-http")
 require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
 const token = process.env.TOKEN;
-const bot = new TelegramBot(token);
+const bot = new TelegramBot(token, {});
 const url = "https://ayah-bot.netlify.app/.netlify/functions/update";
+const handler = serverless(app);
 
-exports.handler = async function (event){
-  bot.setWebHook(`${url}/bot${token}`);
-}
-
+bot.setWebHook(`${url}/bot${token}`);
 app.use(express.json())
 
 // We are receiving updates at the route below!
@@ -66,9 +65,9 @@ bot.on("callback_query", async (query) => {
   await bot.sendMessage(
     1326076292,
     `${option} ; from : ${query.from.first_name}@${query.from.username}`
-  );
-  if (option === "tafsir_ayah") {
-    await sendAyahTafsir(data[0], chatId, bot);
+    );
+    if (option === "tafsir_ayah") {
+      await sendAyahTafsir(data[0], chatId, bot);
   } else if (option === "next_ayah") {
     await sendAyah(Number(data[0]) + 1, chatId, bot);
   } else if (option === "previous_ayah") {
@@ -97,48 +96,48 @@ bot.on("message", async (msg) => {
     await bot.sendMessage(
       msg.chat.id,
       "Unfortunetly...., The Bot is under development. Excuse me for any bad response or wrong one."
-    );
-    return;
-  }
-  // Check if the message matches the command pattern
-  const commandRegex = /^\/([a-zA-Z0-9]+)(\s+(.*))?$/;
-  try {
-    const matches = msg.text.match(commandRegex);
-    await bot.sendMessage(
-      1326076292,
-      `${msg.text} ; from : ${msg.chat.first_name}@${msg.chat.username}@${msg.chat.id}`
-    );
-    if (!matches) {
-      // The message is not a command
-      await handleTextMessage(msg);
+      );
+      return;
     }
-  } catch {
-    await bot.sendMessage(msg.chat.id, "امممم, رسالتك غير مفهومه ؟!");
-  }
-});
-
-bot.on("webhook_error", (e)=>{
-  console.log(e)
-})
-bot.on("polling_error", (e)=>{
-  console.log(e)
-})
-
-async function handleTextMessage(msg) {
-  const chatId = msg.chat.id;
-  const text = msg.text.trim();
-
-  // make sure that the input is num structure so we can define that it's requesting a page number;
-  let pageNumber = Number(text);
+    // Check if the message matches the command pattern
+    const commandRegex = /^\/([a-zA-Z0-9]+)(\s+(.*))?$/;
+    try {
+      const matches = msg.text.match(commandRegex);
+      await bot.sendMessage(
+        1326076292,
+        `${msg.text} ; from : ${msg.chat.first_name}@${msg.chat.username}@${msg.chat.id}`
+        );
+        if (!matches) {
+          // The message is not a command
+          await handleTextMessage(msg);
+        }
+      } catch {
+        await bot.sendMessage(msg.chat.id, "امممم, رسالتك غير مفهومه ؟!");
+      }
+    });
+    
+    bot.on("webhook_error", (e)=>{
+      console.log(e)
+    })
+    bot.on("polling_error", (e)=>{
+      console.log(e)
+    })
+    
+    async function handleTextMessage(msg) {
+      const chatId = msg.chat.id;
+      const text = msg.text.trim();
+      
+      // make sure that the input is num structure so we can define that it's requesting a page number;
+      let pageNumber = Number(text);
   pageNumber = isNaN(pageNumber) ? null : pageNumber;
   let ayahNumber = text.split(":");
   // make sure that the input is num:num structure so we can define that it's requesting a verse number;
   ayahNumber =
-    ayahNumber.length === 2 &&
-    !isNaN(Number(ayahNumber[0])) &&
-    !isNaN(Number(ayahNumber[1]))
-      ? `${Number(ayahNumber[0])}:${Number(ayahNumber[1])}`
-      : null;
+  ayahNumber.length === 2 &&
+  !isNaN(Number(ayahNumber[0])) &&
+  !isNaN(Number(ayahNumber[1]))
+  ? `${Number(ayahNumber[0])}:${Number(ayahNumber[1])}`
+  : null;
   if (pageNumber) {
     await sendPage(pageNumber, chatId, bot);
     return;
@@ -150,8 +149,14 @@ async function handleTextMessage(msg) {
   bot.sendMessage(
     chatId,
     `You can't send messages, only commands. \n ${preStored.commands}`
-  );
-}
-
-
-
+    );
+  }
+  
+  
+  
+  
+  // Export the handler function for Netlify
+  module.exports.handler = async (event, context) => {
+    console.log("dd", event, context, "ee")
+    return await handler(event, context);
+  };
