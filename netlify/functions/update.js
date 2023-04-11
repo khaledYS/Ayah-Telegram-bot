d38c098280ed3590e67e79ab30a12baef01c847d
Al pacino <ammaryasser7654@gmail.com>
2023-04-11 12:02:05 +0000

    updated here

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -2,10 +2,13 @@ const { handleTelegram } = require("../..");
 // Create a bot instance
 
 exports.handler = async (req) =>{
-    const message = req.body.message;
-    console.log(req.body, "here")
+    let message = req.body.message;
+    message = JSON.stringify(message);
+    message = JSON.parse(message);
+    console.log(message, "here")
     try{
         await handleTelegram(message)
+        console.log("finished the one")
     }catch (e){
         console.log(e)
     }finally{
